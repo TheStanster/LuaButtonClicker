bf33d96d422b4d6b4f5e3cb4573687c1ee23afd2
Stan Valster <svalster@sogyo.nl>
2020-08-06 16:20:29 +0200

    Added a counter for misses, an accuracy rating to the results, and made the button switch color depending on the previous click being a hit or miss

--- src/main.lua
+++ src/main.lua
@@ -3,10 +3,15 @@ function love.load()
   button.x = 200
   button.y = 200
   button.size = 50
+  button.r = 1
+  button.g = 0
 
+  missed = 0
   score = 0
-  time = 0
+  accuracy = 0
+
   timeLimit = 5
+  time = timeLimit
   myFont = love.graphics.newFont(20)
 
   windowX = love.graphics.getWidth()
@@ -14,13 +19,13 @@ function love.load()
 end
 
 function love.update(dt)
-  if math.ceil(time) < timeLimit then
-    time = time + (1/60)
+  if math.ceil(time) > 0 then
+    time = time - (1/60)
   end
 end
 
 function love.draw()
-  love.graphics.setColor(1, 0, 0)
+  love.graphics.setColor(button.r, button.g, 0)
   love.graphics.circle("fill", button.x, button.y, button.size)
 
   love.graphics.setFont(myFont)
@@ -28,36 +33,61 @@ function love.draw()
   love.graphics.print(score)
   love.graphics.print("Time elapsed: " , 100, 0)
   love.graphics.print(math.ceil(time) , 250, 0)
-  if math.ceil(time) == timeLimit then
+  if math.ceil(time) == 0 then
     love.graphics.clear()
     timeUp()
   end
 end
 
 function love.mousepressed(x, y, butt, isTouch)
-  d = ((y - button.y)^2+(x - button.x)^2)^0.5
+  -- d = ((y - button.y)^2+(x - button.x)^2)^0.5
+  d = distanceBetween(button.x, button.y, love.mouse.getX(), love.mouse.getY())
   if butt == 1 then
-    if d >= -50 and d <= 50 then
+    if d <= button.size then
       score = score + 1
-      buttonRefresh()
+      buttonRefreshScored()
+    else
+      missed = missed + 1
+      buttonRefreshMissed()
     end
   end
 end
 
-function buttonRefresh()
-  px = windowX-50
-  py = windowY-50
-  button.x = love.math.random(50, px)
-  button.y = love.math.random(60, py)
+function buttonRefreshScored()
+  pointX = windowX - button.size
+  pointY = windowY - button.size
+  button.x = love.math.random(button.size, pointX)
+  button.y = love.math.random(button.size + 10, pointY)
+  button.r = 0
+  button.g = 1
+end
+
+function buttonRefreshMissed()
+  pointX = windowX - button.size
+  pointY = windowY - button.size
+  button.x = love.math.random(button.size, pointX)
+  button.y = love.math.random(button.size + 10, pointY)
+  button.r = 1
+  button.g = 0
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  return ((y2 - y1)^2+(x2 - x1)^2)^0.5
 end
 
 function timeUp()
+  if score > 0 then
+    accuracy = (score / (score + missed))*100
+  end
   px1 = ((windowX)/2)-250
   px2 = ((windowX)/2)+50
   py1= ((windowY)/2)
   py2= ((windowY)/2)
-  love.graphics.print("Time's up! Your score is: " , px1, py1)
+  love.graphics.print("Time's up! Your score is: ", px1, py1)
   love.graphics.print(score , px2, py2)
+  love.graphics.print("Your accuracy was: ", px1, py1 + 20)
+  love.graphics.print(accuracy , px2, py2 + 20)
+  love.graphics.print("%" , px2 + 40, py2 + 20)
   function love.mousepressed(x, y, butt, isTouch)
     if butt == 1 then
       love.window.close()
